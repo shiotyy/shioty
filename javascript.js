25fb6460551a89193a782392efff0a41df453233
shiotyy <92772842+shiotyy@users.noreply.github.com>
2021-12-21 03:24:19 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.getElementById("clicked").addEventListener("click", myFunction);
+
+	function myFunction() {
+  document.getElementById("clicked").innerHTML = "nice! now click the photos below :)";
+}
+
+
---
+++ javascript2.js
@@ -0,0 +1,5 @@
+document.getElementById("clicked").addEventListener("click", myFunction);
+
+	function myFunction() {
+  document.getElementById("clicked").innerHTML = "yep! that's really me.";
+}
